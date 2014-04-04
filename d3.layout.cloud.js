a750c4d40a2b344e90294d03c30a11a9764c28a6
Michael Thome <mthome@bbn.com>
2014-04-04 12:39:02 -0400

    add a cloud.random(f) call to allow specification of constant, seeded, or standard (default) random number generator for stable clouds

--- d3.layout.cloud.js
+++ d3.layout.cloud.js
@@ -11,6 +11,7 @@
         rotate = cloudRotate,
         padding = cloudPadding,
         spiral = archimedeanSpiral,
+        random = Math.random,
         words = [],
         timeInterval = Infinity,
         event = d3.dispatch("word", "end"),
@@ -45,8 +46,8 @@
             d;
         while (+new Date - start < timeInterval && ++i < n && timer) {
           d = data[i];
-          d.x = (size[0] * (Math.random() + .5)) >> 1;
-          d.y = (size[1] * (Math.random() + .5)) >> 1;
+          d.x = (size[0] * (random() + .5)) >> 1;
+          d.y = (size[1] * (random() + .5)) >> 1;
           cloudSprite(d, data, i);
           if (d.hasText && place(board, d, bounds)) {
             tags.push(d);
@@ -85,7 +86,7 @@
           startY = tag.y,
           maxDelta = Math.sqrt(size[0] * size[0] + size[1] * size[1]),
           s = spiral(size),
-          dt = Math.random() < .5 ? 1 : -1,
+          dt = random() < .5 ? 1 : -1,
           t = -dt,
           dxdy,
           dx,
@@ -177,6 +178,12 @@
       return cloud;
     };
 
+    cloud.random = function(x) {
+      if (!arguments.length) return random;
+      random = d3.functor(x);
+      return cloud;
+    };
+
     cloud.fontSize = function(x) {
       if (!arguments.length) return fontSize;
       fontSize = d3.functor(x);
@@ -209,7 +216,7 @@
   }
 
   function cloudRotate() {
-    return (~~(Math.random() * 6) - 3) * 30;
+    return (~~(random() * 6) - 3) * 30;
   }
 
   function cloudPadding() {
