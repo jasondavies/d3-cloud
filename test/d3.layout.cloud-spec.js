5ec2cfb3a31979aa91b48c044676472f3a16ffec
cesine <cesine@yahoo.com>
2015-06-22 22:28:43 -0400

    adding node tests

--- test/d3.layout.cloud-spec.js
+++ test/d3.layout.cloud-spec.js
@@ -1,42 +1,53 @@
-document = require("jsdom").jsdom("<html><head></head><body></body></html>");
-window = document.createWindow();
-navigator = window.navigator;
-CSSStyleDeclaration = window.CSSStyleDeclaration;
+try {
 
-require("../lib/d3/d3");
+  document = require("jsdom").jsdom("<html><head></head><body></body></html>");
+  // window = document.createWindow();
+  // navigator = window.navigator;
+  // CSSStyleDeclaration = window.CSSStyleDeclaration;
 
-var fs = require("fs");
+  var d3 = require("../src/d3.layout.cloud");
 
-var w = 960 * 1,
+  var fs = require("fs");
+
+  var w = 960 * 1,
     h = 600 * 1;
 
-var layout = require("../d3.layout.cloud").cloud()
+  var layout = d3.layout.cloud()
     .padding(0)
     .size([w, h])
     .font("Impact")
-    .text(function(d) { return d.key; })
+    .text(function(d) {
+      return d.key;
+    })
     .on("end", draw);
 
-var start = +new Date;
+  var start = +new Date;
 
-for (var i=0; i<2; i++)
-  parseText(fs.readFileSync("Genesis.txt", "utf8"));
+  for (var i = 0; i < 2; i++)
+    parseText(fs.readFileSync("Genesis.txt", "utf8"));
 
-function parseText(text) {
-  var tags = {};
-  text.split(/\s+/g).forEach(function(word) {
-    word = word.replace(/[^A-Za-z0-9]/g, "");
-    if (word.length <= 3) return;
-    tags[word] = (tags[word] || 0) + 1;
-  });
-  tags = d3.entries(tags).sort(function(a, b) { return b.value - a.value; }).slice(0, 500);
-  var min = +tags[tags.length - 1].value || 1,
+  function parseText(text) {
+    var tags = {};
+    text.split(/\s+/g).forEach(function(word) {
+      word = word.replace(/[^A-Za-z0-9]/g, "");
+      if (word.length <= 3) return;
+      tags[word] = (tags[word] || 0) + 1;
+    });
+    tags = d3.entries(tags).sort(function(a, b) {
+      return b.value - a.value;
+    }).slice(0, 500);
+    var min = +tags[tags.length - 1].value || 1,
       max = +tags[0].value;
-  layout.fontSize(function(d) {
-    return min + (d - min) / (max - min);
-  }).words(tags).start();
-}
+    layout.fontSize(function(d) {
+      return min + (d - min) / (max - min);
+    }).words(tags).start();
+  }
 
-function draw() {
-  console.log(+new Date - start);
+  function draw() {
+    console.log(+new Date - start);
+  }
+
+} catch (e) {
+  console.log(e);
+  console.log(e, e.stack);
 }
