a6d90abe147c8f3e12fdf4803666b651c09773c1
Jason Davies <jason@jasondavies.com>
2022-07-15 23:44:00 +0100

    Update Node.js usage example.
    Fixes #153.

--- examples/node.js
+++ examples/node.js
@@ -1,20 +1,17 @@
-var Canvas = require("canvas");
+const { createCanvas } = require("canvas");
+const cloud = require("d3-cloud");
 
-var cloud = require("../");
-
-var words = ["Hello", "world", "normally", "you", "want", "more", "words", "than", "this"]
+const words = ["Hello", "world", "normally", "you", "want", "more", "words", "than", "this"]
     .map(function(d) {
       return {text: d, size: 10 + Math.random() * 90};
     });
 
 cloud().size([960, 500])
-    .canvas(function() { return new Canvas(1, 1); })
+    .canvas(() => createCanvas(1, 1))
     .words(words)
     .padding(5)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .rotate(() => Math.floor(Math.random() * 2) * 90)
     .font("Impact")
-    .fontSize(function(d) { return d.size; })
-    .on("end", end)
+    .fontSize(d => d.size)
+    .on("end", words => console.log(JSON.stringify(words)))
     .start();
-
-function end(words) { console.log(JSON.stringify(words)); } 
